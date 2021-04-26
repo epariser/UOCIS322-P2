f8e560ff98129c3fb93d67e5c360bfd98d676d4a
Evan Pariser <epariser@testium.cs.uoregon.edu>
2021-04-26 01:13:52 -0700

    somewhat working app.py

--- web/app.py
+++ web/app.py
@@ -1,14 +1,36 @@
 """
-John Doe's Flask API.
+Evan PAriser's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, render_template, abort, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "UOCIS docker demo!\n"
+    return "UOCIS docker demo\n"
+
+@app.rout(</path:fileName>)
+def requesthandler(fileName):
+    forbidden = ['//','..','~']
+
+    if any(x in fileName for x in forbidden):
+        abort(403)
+    
+    if not (fileName.endswith('.html') or fileName.endswith('.css')):
+        abort(404)
+
+
+    
+
+@app.errorhandler(404)  #file not found error
+def error_404():
+    return render_template('templates', '404.html'), 404
+
+@app.errorhandler(403)  #forbidden error
+def error_403(e):
+    return render_template('templates', '403.htm'), 403
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
